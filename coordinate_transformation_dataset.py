0d5016ddc36b89deea14fb1807298cf92e24bfa0
tripdancer0916 <tripdancer0916@gmail.com>
2021-05-31 08:38:11 +0900

    Fix sigma_posterior

--- coordinate_transformation_dataset.py
+++ coordinate_transformation_dataset.py
@@ -111,10 +111,8 @@ class CoordinateTransform(data.Dataset):
         # target
         sigma_1 = np.sqrt(1 / g_1) * self.uncertainty
         sigma_2 = np.sqrt(1 / g_2) * self.uncertainty
-        # mu_posterior = 2 * ((sigma_1 ** 2) * signal_mu2 +
-        #                     (sigma_2 ** 2) * signal_mu1) / (sigma_1 ** 2 + sigma_2 ** 2)
         mu_posterior = signal_mu1 + signal_mu2
-        sigma_posterior = sigma_1 + sigma_2
+        sigma_posterior = np.sqrt(sigma_1 ** 2 + sigma_2 ** 2)
         target_sample = np.random.normal(mu_posterior, sigma_posterior, 1000)
         a_list = np.linspace(-20, 20, 40) + 0.5
         p_soft = np.zeros(40)
